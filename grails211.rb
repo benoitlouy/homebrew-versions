2a93a233016ee2e07c982ec1123cb82ea2cd898a
Benoit Louy <blouy@ondeckcapital.com>
2013-04-25 14:58:48 -0400

    adding grails211 formula

---
+++ grails211.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Grails21 < Formula
+  homepage 'http://grails.org'
+  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.1.1.zip'
+  sha1 '1beaa46a00ddd11c8c090c926e2f54af85530cc1'
+
+  def install
+    rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
+    prefix.install %w[LICENSE README]
+    libexec.install Dir['*']
+    bin.mkpath
+    Dir["#{libexec}/bin/*"].each do |f|
+      next unless File.extname(f).empty?
+      ln_s f, bin+File.basename(f)
+    end
+  end
+end
